92b9b065c73ffbe8cd8ccd3c4c9a3406b777d971
Dialest <dialest@hotmail.com>
2019-03-04 18:49:30 -0800

    Initialized node. Added inquirer and prompt.  Wrote letter.js.

---
+++ letter.js
@@ -0,0 +1,17 @@
+function Letter(letter) {
+    this.letter = letter;
+    this.guessed = false;
+    this.display = function() {
+        if (this.guessed) {
+            process.stdout.write(letter + " ");
+        }
+        else {
+            process.stdout.write("_ ");
+        }
+    }
+    this.check = function(guess) {
+        if(guess === this.letter) {
+            this.guessed = true;
+        }
+    }
+}
