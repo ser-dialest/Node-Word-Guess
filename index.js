24b33a3ce04c2e6c20aefaead0e8a6be9930886c
Dialest <dialest@hotmail.com>
2019-03-09 18:51:40 -0800

    Answer is 1 of 1500 words instead of butts. Add word completion and failure results.

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 var Word = require("./word");
+var inquirer = require('inquirer');
+var fs = require("fs");
+
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var used = [];
+var wins = 0;
 
-// var butts = new Word("butts");
-// butts.display();
-// butts.guess("t");
-// butts.display();
-// butts.guess("b");
-// butts.display();
-// butts.guess("s");
-// butts.display();
-// butts.guess("u");
-// butts.display();
+// List of possible words
+var words;
+fs.readFile("words.txt", "utf8", function(error, data) {
+    if (error) {
+        return console.log(error);
+    }
+    var string = data.replace(/\r/g, "");
+    words = string.split("\n"); 
+    game();
+});
 
 function game() {
     console.log();
 
-    var answer = new Word("butts");
+    var answer = new Word(words[Math.floor(Math.random() * words.length)]);
     answer.display();
 
-    var inquirer = require('inquirer');
-    inquirer.prompt([
-        {
-            name: "letter",
-            message: "Guess a letter!",
-            type: "input",
-            validate: function validateGuess(letter){
-                return (alphabet.indexOf(letter.toLowerCase()) != -1 && used.indexOf(letter.toLowerCase()) === -1);
+    // reset which letters have been used
+    var used = [];
+
+    function turn() {
+        inquirer.prompt([
+            {
+                name: "letter",
+                message: "Guess a letter!",
+                type: "input",
+                validate: function validateGuess(letter){
+                    return (alphabet.indexOf(letter.toLowerCase()) != -1 && used.indexOf(letter.toLowerCase()) === -1);
+                }
             }
-        }
-    ]).then(function(answers) {
-        var guess = answers.letter.toLowerCase();
-        used.push(guess);
-        console.log();
-        answer.check(guess);
-        answer.display();
-        // console.log(answers.letter);
-        // console.log(typeof answers.letter);
-        // console.log(answers.letter.length);
-
-        
-    });
+        ]).then(function(answers) {
+            var guess = answers.letter.toLowerCase();
+            used.push(guess);
+            console.log();
+            if (answer.check(guess)) {
+                answer.display();
+                console.log("You got it right! Next word!");
+                wins++;
+                game();
+            }
+            else {
+                if (answer.misses > 0) {
+                    answer.display();
+                    turn();
+                }
+                else {
+                    console.log("Game over. You got " + wins + " correct this time.");
+                    console.log("Thanks for playing!!");
+                }
+            };
+        });
+    }
+    turn();
 }
-
-game();
--- word.js
+++ word.js
@@ -16,20 +16,27 @@ function Word(word) {
     }
     this.check = function(guess) {
         var correct = false;
+        var allCorrect = true;
         for (var i = 0; i < this.word.length; i++) {
             if (this.word[i].check(guess)) {
                 correct = true;
+            };
+            if (!this.word[i].guessed) {
+                allCorrect = false;
             };
         }
         if (correct) {
             console.log(clc.green("CORRECT!!!"));
             console.log();
+            return allCorrect;
         }
         else {
             console.log(clc.red("INCORRECT!!!"));
             console.log();
             this.misses--;
             console.log(this.misses + " guesses remaining!!!");
+            console.log();
+            return false;
         }
     }
 };
